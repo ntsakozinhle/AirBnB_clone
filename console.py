2881d294043a70f4c6bfc050043bf627c94b947f
ntsakozinhle <ntsakosambo1@gmail.com>
2024-03-05 20:06:25 -0800

    Console 0.0.1

--- console.py
+++ console.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-"""This module defined the entry point for the AirBnB clone project."""
+"""This module defined the command interpreter for the AirBnB clone project."""
 
-from hbnb_command import HBNBCommand
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class for the AirBnB clone project."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the command interpreter."""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing when empty line is entered"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
