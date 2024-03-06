0f631b7b8ff579ca8281bfeed08b91c8bf4d34ef
ntsakozinhle <ntsakosambo1@gmail.com>
2024-03-05 19:00:20 -0800

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""This module defines the BaseModel class."""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Defines the base model for other classes."""
+
+    def __init__(self):
+        """Initializes instance attributes."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return string representation of BaseModel instance."""
+        return "[{}] ({}) {}".format(
+                type(self).__name__, self.id, self.__dict__
+                )
+
+    def save(self):
+        """Update updated_at attribute with current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return dictionary representation of BaseModel instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+
+    my_model.save()
+    print(my_model)
+
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+
+    print("JSON of my_model:")
+    for key, value in my_model_json.items():
+        print("\t{}: ({}) - {}".format(key, type(value), value))
