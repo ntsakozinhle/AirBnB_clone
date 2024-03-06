02e41bede36652804a0edb3c5c916b8095606b39
ntsakozinhle <ntsakosambo1@gmail.com>
2024-03-05 19:11:46 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ from datetime import datetime
 class BaseModel:
     """Defines the base model for other classes."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes instance attributes."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return string representation of BaseModel instance."""
@@ -35,14 +42,21 @@ if __name__ == "__main__":
     my_model = BaseModel()
     my_model.name = "My First Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-
-    my_model.save()
-    print(my_model)
-
+    print(type(my_model.created_at))
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
-
     print("JSON of my_model:")
     for key, value in my_model_json.items():
         print("\t{}: ({}) - {}".format(key, type(value), value))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
